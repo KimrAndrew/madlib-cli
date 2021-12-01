eb2dec97e6baed68b9e34c86acb854a48fb0a703
KimrAndrew <andrewrk1997@gmail.com>
2021-12-01 15:39:10 -0800

    parse_template able to return stripped template

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,10 +1,19 @@
+import re
+
 def merge():
     pass
 
 def read_template(path):
+    """
+    Takes in a file path and returns the file contents as a string
+    """
+
     with open(path,'r') as f:
         contents = f.read()
         return contents
 
 def parse_template(file_contents):
-    pass
+    regex = r'{.*?}'
+    return re.sub(regex,'{}',file_contents)
+
+print(parse_template(read_template('assets/dark_and_stormy_night_template.txt')))
